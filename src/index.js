c3393bdbfe243bc70bb900cf999ec11d9c5800e6
Hazmi <whitespaces@gmail.com>
2018-11-07 23:51:43 +0700

    restructure files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route  } from "react-router-dom";
+import Header from './components/header';
+import Home from './components/home';
+import Jelajah from './components/jelajah';
+import Pencarian from './components/pencarian';
 import './index.css';
-import App from './app';
 import * as serviceWorker from './serviceWorker';
+
+const App = () => (
+  <Router>
+    <div>
+      <Header />
+      <Route path="/" exact component={Home} />
+      <Route path="/jelajah/" component={Jelajah} />
+      <Route path="/pencarian/" component={Pencarian} />
+    </div>
+  </Router>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
