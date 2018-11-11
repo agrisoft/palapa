f9f1a9a1b8012743a0d3df9ba2811f6b35f42d3f
Hazmi <whitespaces@gmail.com>
2018-11-11 19:39:11 +0700

    update react to enable hooks

--- src/components/jelajah/index.js
+++ src/components/jelajah/index.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import './index.css';
 
-class Jelajah extends Component {
-  render() {
-    return (
-      <div>
-        Jelajah
-      </div>
-    );
-  }
-}
+const Jelajah = () => {
+  const [height, setHeight] = useState(window.innerHeight);
+  useEffect(() =>{
+    const handleResize = () => setHeight(window.innerHeight);
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  });
+  return (
+    <div className="jelajah" style={{ height }}>
+      Jelajah: {height}
+    </div>
+  );
+};
 
 export default Jelajah;
