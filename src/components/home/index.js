d5e2b6625055bbe8a5f8d8e5973588d50e3d0fb7
Hazmi <whitespaces@gmail.com>
2018-11-14 21:38:49 +0700

    update peta in search panel at home page

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Map, TileLayer, ZoomControl } from 'react-leaflet';
+import Select from 'react-select';
 
 import { Carousel } from './components/carousel';
 import { Berita } from './components/berita';
@@ -14,6 +15,14 @@ import dataWalidata from './data/walidata.json';
 import dataKategori from './data/kategori.json';
 import dataTerbaru from './data/terbaru.json';
 
+const colourStyles = {
+  control: styles => ({ ...styles,
+    borderRadius: 0,
+    height: 60,
+    backgroundColor: '#f1f1f1',
+  }),
+};
+
 export const Home = () => {
   const [isAdvanceActive, setAdvanceActive] = useState(false);
   let searchClassName = 'search';
@@ -25,8 +34,11 @@ export const Home = () => {
     <div className="home">
       <div className="banner">
         <Carousel />
-        <div className="banner__overlay">
-          <p className="banner__intro">
+        <div className="banner__overlay" />
+      </div>
+      <div className="home__search">
+        <div className="container">
+          <p className="home__search__intro">
               Geoportal PALAPA merupakan salah satu simpul Jaringan Informasi Geospasial Nasional (JIGN). 
               Data dan informasi geospasial disediakan dalam bentuk GIS web services dan dapat ditelusuri 
               keberadaan datanya.
@@ -34,14 +46,35 @@ export const Home = () => {
           <div className={searchClassName}>
             <span
               className="search__advanced-link"
-              onClick={() => setAdvanceActive(!isAdvanceActive)}
+              onClick={() => {
+                window.dispatchEvent(new Event('resize'));
+                setAdvanceActive(!isAdvanceActive);
+              }}
             >
               <span className="icon-settings" />
             </span>
             <a href="#submit-search" className="search__submit">
               <span className="icon-magnifier" />
             </a>
+            <div className="search__select-wrapper">
+              <div className="search__select-kategori">
+                <Select placeholder="Semua Kategori" options={dataKategori.data} styles={colourStyles} />
+              </div>
+              <div className="search__select-walidata">
+                <Select placeholder="Semua Walidata" options={dataWalidata.data} styles={colourStyles} />
+              </div>
+            </div>
             <input type="text" placeholder="Kata Kunci" className="search__input" />
+            <div className="search__map-wrapper" style={{ height: isAdvanceActive ? 650 : 0}}>
+              <div className="search__map">
+                <Map center={[ -6.175985, 106.827313 ]} zoom={12} zoomControl={false}>
+                  <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  />
+                  <ZoomControl position="topleft" />
+                </Map>
+              </div>
+            </div>
           </div>
         </div>
       </div>
