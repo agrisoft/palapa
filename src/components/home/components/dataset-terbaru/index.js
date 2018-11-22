13a3a8f5b1958c44584f42c70e44bc9edc629300
Hazmi <whitespaces@gmail.com>
2018-11-22 21:29:57 +0700

    add config + implement API for dataset terbaru

--- src/components/home/components/dataset-terbaru/index.js
+++ src/components/home/components/dataset-terbaru/index.js
@@ -1,23 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import fetch from 'node-fetch';
+import PropagateLoader from 'react-spinners/PropagateLoader';
 import Dataset from '../../../../library/dataset';
 import './index.scss';
+import config from '../../../../config';
 
-export const DatasetTerbaru = ({ data }) => (
-  <div className="dataset-terbaru">
-    <div className="container dataset-terbaru__wrapper">
-      <h2 className="dataset-terbaru__header">
-        <span className="dataset-terbaru__header__line" />
-        Dataset Terbaru
-      </h2>
-      <div className="dataset-terbaru__list">
-        {data.map((item) => (
-          <div className="dataset-terbaru__list__item">
-            <Dataset {...item} />
-          </div>
-        ))}
+export const DatasetTerbaru = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+
+  fetch(`${config.api}/listmetalayer`)
+    .then(res => res.json())
+    .then(json => {
+      let data = [];
+      const recentData = json.slice(0, 4);
+      recentData.map((item) => {
+        data.push({
+          title: item.title,
+          kategori: item.keywords,
+          image: `${config.host}/gsassets/thumbnails/` + item.identifier.replace(/\:/,'-') + '.png',
+          author: item.workspace,
+        });
+      });
+      setData(data);
+    });
+
+  if (isLoading) {
+    return (
+      <div className="dataset-terbaru" style={{ textAlign: "center" }}>
+        <PropagateLoader
+          className={{
+            width: 1,
+            display: 'block',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+          }}
+          sizeUnit={"px"}
+          size={10}
+          color={'#e87171'}
+          loading={true}
+        />
+      </div>
+    );
+  }
+  if (data.length < 1) return null;
+  return (
+    <div className="dataset-terbaru">
+      <div className="container dataset-terbaru__wrapper">
+        <h2 className="dataset-terbaru__header">
+          <span className="dataset-terbaru__header__line" />
+          Dataset Terbaru
+        </h2>
+        <div className="dataset-terbaru__list">
+          {data.map((item) => (
+            <div className="dataset-terbaru__list__item">
+              <Dataset {...item} />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+}
 
 export default DatasetTerbaru;
---
+++ src/config/index.js
@@ -0,0 +1,5 @@
+export const config = {
+  host: 'http://palapa.agrisoft-cb.com/',
+  api: 'http://palapa.agrisoft-cb.com/api',
+};
+export default config;
