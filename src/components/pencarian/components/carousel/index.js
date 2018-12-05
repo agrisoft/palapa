b2717b7699c8a0b03ab1ba24d8cd15db786cc6a7
Hazmi <whitespaces@gmail.com>
2018-12-05 20:34:53 +0700

    remove dummy banner in pencarian

--- src/components/pencarian/components/carousel/index.js
+++ src/components/pencarian/components/carousel/index.js
@@ -4,9 +4,6 @@ import Slider from "react-slick";
 import './index.css';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
-import image1 from './images/1.jpg';
-import image2 from './images/2.jpg';
-import image3 from './images/3.jpg';
 
 const settings = {
   dots: false,
